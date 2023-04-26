0469187db700190709d2bc505908db760ce88da3
Hitori <52887342+PS-Hitori@users.noreply.github.com>
2023-04-27 00:41:20 +0800

    Update AddScenesToBuild.cs

--- AddScenesToBuild.cs
+++ AddScenesToBuild.cs
@@ -1,40 +1,99 @@
-// Author: Hitori
-// Date: 26th April 2023 (Wednesday) 
-// Description: This script adds all scene files in the "Assets/Scenes" directory to the build settings in Unity. 
-// It can be accessed through the "Tools" menu and is useful for quickly adding multiple scenes to the build settings.
-// Usage: Add this script to the "Editor" folder in your project. Then, go to "Tools" in the Unity menu bar and click "QOL Addons/Add All Scenes to Build Settings".
-
 using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
+
 public class AddScenesToBuild
 {
-    // Constant for the scenes directory
-    private const string ScenesDirectory = "Assets/Scenes"; // Let us assume this is the default directory, change it if your Scenes are in a different directory
+   // Configuration settings
+    private const string ScenesDirectoryKey = "ScenesDirectory";
+    private const string DefaultScenesDirectory = "Assets/Scenes";
+    private const string SceneFilter = "t:Scene";
 
-    [MenuItem("Tools/QOL Addons/Add All Scenes to Build Settings")]
-    public static void AddScenesToBuild()
+    [MenuItem("Tools/Add-ons/Add Scenes to Build Settings")]
+    private static void AddScenesToBuildSettings()
     {
-        // Get all scene files in the scenes directory using AssetDatabase and the ScenesDirectory constant
-        string[] sceneBuilds = AssetDatabase.FindAssets("t:Scene", new[] { ScenesDirectory });
-        
-        // If there are no scenes in the scenes directory, log an error and return
-        if (sceneBuilds.Length == 0)
+        try
         {
-            Debug.LogError("No scenes found in " + ScenesDirectory + ".");
-            return;
+            // Find all scenes in the scenes directory
+            string scenesDirectory = EditorPrefs.GetString(ScenesDirectoryKey, DefaultScenesDirectory);
+            string[] sceneGUIDs = AssetDatabase.FindAssets(SceneFilter, new[] { scenesDirectory });
+
+            // Check if there are any scenes in the scenes directory
+            if (sceneGUIDs.Length == 0)
+            {
+                Debug.LogError($"No scene files found in {scenesDirectory}. " +
+                               $"Add at least one scene file to the directory and try again.");
+                return;
+            }
+
+            // Create the build scene array
+            EditorBuildSettingsScene[] buildScenes = new EditorBuildSettingsScene[sceneGUIDs.Length];
+
+            for (int i = 0; i < sceneGUIDs.Length; i++)
+            {
+                // Get the path of the scene file
+                string scenePath = AssetDatabase.GUIDToAssetPath(sceneGUIDs[i]);
+
+                // Add the scene to the build settings
+                buildScenes[i] = new EditorBuildSettingsScene(scenePath, true);
+            }
+
+            // Check if there are existing scenes in the build settings
+            if (EditorBuildSettings.scenes.Length > 0)
+            {
+                // Show warning message to the user
+                bool replaceScenes = EditorUtility.DisplayDialog("Warning",
+                    "There are existing scenes in the build settings. " +
+                    "Adding new scenes may replace existing scenes. " +
+                    "Do you want to continue?",
+                    "Yes", "No");
+
+                // If the user wants to replace the scenes, proceed
+                if (replaceScenes)
+                {
+                    switch (EditorUtility.DisplayDialogComplex("Add Scenes to Build Settings",
+                        "What do you want to do with the new scenes?",
+                        "Append", "Replace", "Cancel"))
+                    {
+                        case 0: // Append
+                                // Get all scenes in the build settings
+                            List<EditorBuildSettingsScene> allScenes = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
+                            foreach (EditorBuildSettingsScene buildScene in buildScenes)
+                            {
+                                // Check if a scene with the same path as the new scene already exists in the build settings
+                                EditorBuildSettingsScene existingScene = allScenes.Find(scene => scene.path == buildScene.path);
+                                if (existingScene != null)
+                                {
+                                    // Remove the existing scene before adding the new scene
+                                    allScenes.Remove(existingScene);
+                                }
+                                allScenes.Add(buildScene);
+                            }
+                            EditorBuildSettings.scenes = allScenes.ToArray();
+                            Debug.Log($"Added {buildScenes.Length} scenes to the build settings.");
+                            break;
+                        case 1: // Replace
+                                // Set the build scenes in the EditorBuildSettings
+                            EditorBuildSettings.scenes = buildScenes;
+                            Debug.Log($"Replaced existing scenes in the build settings with {sceneGUIDs.Length} new scenes.");
+                            break;
+                        case 2: // Cancel
+                                // Do nothing
+                            Debug.Log("Cancelled adding scenes to build settings.");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                // Set the build scenes in the EditorBuildSettings
+                EditorBuildSettings.scenes = buildScenes;
+                Debug.Log($"Added {sceneGUIDs.Length} scenes to the build settings.");
+            }
         }
-
-        // Create the build scene array
-        EditorBuildSettingsScene[] buildScenes = new EditorBuildSettingsScene[sceneBuilds.Length];
-        for(int sceneIndex = 0; sceneIndex < sceneBuilds.Length; sceneIndex++){
-            string scenePath = AssetDatabase.GUIDToAssetPath(sceneBuilds[sceneIndex]);
-            buildScenes[sceneIndex] = new EditorBuildSettingsScene(scenePath, true);
+        catch (System.Exception ex)
+        {
+            Debug.LogError($"An error occurred while adding scenes to build settings: {ex.Message}");
         }
-
-        // Set the build scenes in the EditorBuildSettings
-        EditorBuildSettings.scenes = buildScenes;
-
-        // Log the number of scenes added to the build settings
-        Debug.Log("Added " + sceneBuilds.Length + " scenes to the build settings.");
     }
 }
